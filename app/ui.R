674e4b197f4c49c03d52b44e4285a73469b3efda
keliu0 <98298544+keliu0@users.noreply.github.com>
2022-02-23 12:39:03 -0500

    Add files via upload

--- app/ui.R
+++ app/ui.R
@@ -1,182 +1,23 @@
 
-if (!require("shiny")) {
-  install.packages("shiny")
-  library(shiny)
-}
-if (!require("shinyWidgets")) {
-  install.packages("shinyWidgets")
-  library(shinyWidgets)
-}
-if (!require("shinydashboard")) {
-  install.packages("shinydashboard")
-  library(shinydashboard)
-}
-if (!require("leaflet")) {
-  install.packages("leaflet")
-  library(leaflet)
-}
-if (!require("leaflet.extras")) {
-  install.packages("leaflet.extras")
-  library(leaflet.extras)
-}
-
-shinyUI(dashboardPage(
-  dashboardHeader(title="COVID-19's Impact on NYC Businesses", 
-                  titleWidth=400),
+fluidPage(
   
-  # SideBar content
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Home", tabName="home", icon=icon("home")),
-      menuItem("Map", tabName="map", icon=icon("map")),
-      menuItem("Interactive Plots", tabName="plots", icon=icon("signal"),
-               startExpanded=TRUE,
-               menuSubItem("New application", tabName="new_app"),
-               menuSubItem("Processing Time", tabName="proc_time"),
-               menuSubItem("Passing Rate", tabName="pass_rate")
-               #ADD CONTENT XXXXXXXXXXXXXX
-               ),
-      menuItem("Findings", tabName="findings", icon=icon("star"))
-      
-    )
+  titlePanel("Time Series Analysis"),
+  
+  sidebarPanel(
+    
+    selectInput('input1', 'Year',
+                choices = c('All','2017','2018','2019','2020','2021','2022'),
+                selected = 'All'),
+    selectInput('input2', "License Type",
+                choices = c('All','Business','Individual'),
+                selected = 'All'),
+    selectInput('input3', "Category",
+                choices = c('All','Application or Renew','Application Categoty'),
+                selected = 'All'),
+
   ),
   
-  # Body content
-  dashboardBody(
-    tabItems(
-      #-----------home------------
-      tabItem(tabName="home",
-              fluidPage(
-                h2(strong("Title XXXXXXXXX"), align="center"),
-                h3("Team Members XXXXXXXXXXX", align="center")
-                #ADD CONTENT XXXXXXXXXXXXXX
-              )
-      ),
-      
-      #-----------map------------
-      tabItem(tabName="map",
-              fluidPage(
-                h3("XXXXXXXXXXX", align="center")
-                #ADD CONTENT XXXXXXXXXXXXXX
-              )
-      ),
-      
-      #---------new_app----------
-      tabItem(tabName="new_app",
-              fluidPage(
-                h3("XXXXXXXXXXX", align="center")
-                #ADD CONTENT XXXXXXXXXXXXXX
-              )
-      ),
-      
-      #---------proc_time----------
-      tabItem(tabName="proc_time",
-              fluidPage(
-                sidebarLayout(
-                  sidebarPanel(
-                    radioButtons("app_or_renew", 
-                                 label=h3("Type of Application"),
-                                 choices=list("All"=1, "New Application"=2,
-                                                 "Renewal"=3)
-                                ),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    radioButtons("category", label=h3("Business Category"),
-                                 choices=list("Home Improvement Contractor",
-                                              "Tobacco Retail Dealer",
-                                              "Secondhand Dealer - General",
-                                              "Electronic Cigarette Dealer",
-                                              "Laundries",
-                                              "Electronics Store",
-                                              "Stoop Line Stand",
-                                              "Garage",
-                                              "Electronic & Appliance Service",
-                                              "Dealer In Products",
-                                              "Pawnbroker",
-                                              "Secondhand Dealer - Auto")),
-                    width=3
-                  ),
-                mainPanel(
-                  h3("All Businesses", align="center"),
-                  span(textOutput("val_proc_time_1"), style="font-size: 18px"),
-                  plotOutput("plot_proc_time_1"),
-                  hr(),
-                  h3("Business-type Specific", align="center"),
-                  span(textOutput("val_proc_time_2"), style="font-size: 18px"),
-                  plotOutput("plot_proc_time_2")
-                ))
-              )
-      ),
-      
-      #---------pass_rate----------
-      tabItem(tabName="pass_rate",
-              fluidPage(
-                sidebarLayout(
-                  sidebarPanel(
-                    radioButtons("app_or_renew_1", 
-                                 label=h3("Type of Application"),
-                                 choices=list("All"=1, "New Application"=2,
-                                              "Renewal"=3),
-                                 selected=1),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    br(),
-                    radioButtons("category_1", label=h3("Business Category"),
-                                 choices=list("Home Improvement Contractor",
-                                              "Tobacco Retail Dealer",
-                                              "Secondhand Dealer - General",
-                                              "Electronic Cigarette Dealer",
-                                              "Laundries",
-                                              "Electronics Store",
-                                              "Stoop Line Stand",
-                                              "Garage",
-                                              "Electronic & Appliance Service",
-                                              "Dealer In Products",
-                                              "Pawnbroker",
-                                              "Secondhand Dealer - Auto"),
-                                 selected="Home Improvement Contractor"),
-                    width=3
-                  ),
-                  mainPanel(
-                    h3("All Businesses", align="center"),
-                    span(textOutput("val_pass_rate_1"), style="font-size: 18px"),
-                    plotOutput("plot_pass_rate_1"),
-                    hr(),
-                    h3("All Businesses", align="center"),
-                    span(textOutput("val_pass_rate_2"), style="font-size: 18px"),
-                    plotOutput("plot_pass_rate_2")
-                  ))
-              )
-      ),
-      
-      #---------findings----------
-      tabItem(tabName="findings",
-              fluidPage(
-                h3("XXXXXXXXXXX", align="center")
-                #ADD CONTENT XXXXXXXXXXXXXX
-              )
-      )
-    )
-  )
-  
+  mainPanel(
+    plotOutput('plot')
   )
 )
