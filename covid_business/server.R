d1742b3e234ec3c42bb7f3a539aea8f3ede747b3
JacobGump <sg4010@columbia.edu>
2022-02-22 22:26:31 -0500

    add heatmap

--- covid_business/server.R
+++ covid_business/server.R
@@ -27,25 +27,97 @@ if (!require("leafsync")) {
     library(leafsync)
 }
 
-licence_df = read.csv('../data/License_Applications.csv')
-licence_df = licence_df[1: 100, ]
+if (!require("mapdeck")) {
+    install.packages("mapdeck")
+    library(mapdeck)
+}
+
+if (!require("tidyverse")) {
+    install.packages("tidyverse")
+    library(tidyverse)
+}
+
+if (!require("htmltools")) {
+    install.packages("htmltools")
+    library(htmltools)
+}
+
+if (!require("leaflet.extras")) {
+    install.packages("leaflet.extras")
+    library(leaflet.extras)
+}
+
+heatPlugin <- htmlDependency("Leaflet.heat", "99.99.99",
+             src = c(href = "http://leaflet.github.io/Leaflet.heat/dist/"),
+             script = "leaflet-heat.js")
+
+registerPlugin <- function(map, plugin) {
+    map$dependencies <- c(map$dependencies, list(plugin))
+    map}
+
+
+
+data_raw = read.csv('../data/License_Applications.csv')
+df = data_raw %>%
+    select(ID="Application.ID", l_type="License.Type", 
+           app_or_renew="Application.or.Renewal", status="Status",
+           s_date="Start.Date", e_date="End.Date",
+           category="License.Category", app_cat="Application.Category",
+           city="City", state="State", zip="Zip", longitude="Longitude", 
+           latitude="Latitude") %>%
+    drop_na() %>%
+    mutate(s_date = 
+               as.Date(s_date, format = "%m/%d/%Y")) %>%
+    mutate(e_date = 
+               as.Date(e_date, format = "%m/%d/%Y")) %>%
+    filter(state=="NY") %>% 
+    filter(s_date >= as.Date("01/01/2017",format = "%m/%d/%Y")) %>%
+    filter(s_date < e_date)
 
 shinyServer(function(input, output) {
-
-    output$map <- renderLeaflet({
-
-        map <- licence_df %>%
-            leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
-            addTiles() %>%
-            addProviderTiles("CartoDB.Positron",
-                            options = providerTileOptions(noWrap = TRUE)) %>%
-            setView(-73.9834,40.7504,zoom = 12)
-
-        map %>%
-            addMarkers(
-                lng=~Longitude,
-                lat=~Latitude
-            )
-
+    output$left_map <- renderLeaflet(
+        {
+            left_df = df %>%
+                filter(s_date <= as.Date("05/31/2019",format = "%m/%d/%Y"))
+            if(input$adjust_status != "All"){
+                left_df = left_df %>%
+                    filter(status == input$adjust_status)
+            }
+            if(input$adjust_type != "All"){
+                left_df = left_df %>%
+                    filter(app_or_renew == input$adjust_type)
+            }
+            
+        
+            left_df %>%
+                leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
+                addTiles() %>%
+                addProviderTiles(providers$CartoDB) %>%
+                setView(-73.9834,40.7504,zoom = 12) %>%
+                addWebGLHeatmap(~longitude, ~latitude, size = 200, alphaRange = 0.5, intensity = 0.1)
     })
+    
+    output$right_map <- renderLeaflet(
+        {
+            right_df = df %>%
+                filter(s_date >= as.Date("04/30/2020",format = "%m/%d/%Y"))
+            if(input$adjust_status != "All"){
+                right_df = right_df %>%
+                    filter(status == input$adjust_status)
+            }
+            
+            if(input$adjust_type != "All"){
+                right_df = right_df %>%
+                    filter(app_or_renew == input$adjust_type)
+            }
+            
+            
+            
+            right_df %>%
+                leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
+                addTiles() %>%
+                addProviderTiles(providers$CartoDB) %>%
+                setView(-73.9834,40.7504,zoom = 12) %>%
+                addWebGLHeatmap(~longitude, ~latitude, size = 200, alphaRange = 0.5, intensity = 0.1)
+        })
 })
--- covid_business/ui.R
+++ covid_business/ui.R
@@ -26,9 +26,46 @@ shinyUI(
                          icon = icon("map-marker-alt"), #choose the icon for
                          div(class = 'outer',
                         # side by side plots
-                        leafletOutput("map",width="100%",height=1200)
-                            ) #Maps - Div closing
-                        ) #tabPanel maps closing
+                        fluidRow(
+                          splitLayout(cellWidths = c("50%", "50%"), 
+                                      leafletOutput("left_map",width="100%",height=1200),
+                                      leafletOutput("right_map",width="100%",height=1200))),
+                        
+                        absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
+                                      top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
+                                      tags$h4('License Application Comparison'), 
+                                      tags$br(),
+                                      prettyRadioButtons(
+                                        inputId = "adjust_status",
+                                        label = "Status:", 
+                                        choices = c("Issued", 
+                                                    "Denied",
+                                                    "All"),
+                                        inline = TRUE, 
+                                        status = "danger",
+                                        fill = TRUE
+                                      ),
+                                      prettyRadioButtons(
+                                        inputId = "adjust_type",
+                                        label = "Type:", 
+                                        choices = c("Renewal", 
+                                                    "Application",
+                                                    "All"),
+                                        inline = TRUE, 
+                                        status = "danger",
+                                        fill = TRUE
+                                      ),
+                                      # selectInput('adjust_weather',
+                                      #             label = 'Adjust for Weather',
+                                      #             choices = c('Yes','No'), 
+                                      #             selected = 'Yes'
+                                      #             ),
+                                      style = "opacity: 0.80"
+                                      
+                        ), #Panel Control - Closing
+                      ) #Maps - Div closing
+                  ) #tabPanel maps closing
+                
    
 
 
